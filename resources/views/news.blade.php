7d132c9c840b608343f42cc2f7814d5d1365a5c8
muhammadshiva <shivamuhammad07@gmail.com>
2021-11-15 21:55:33 +0700

    init project

---
+++ resources/views/news.blade.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <title>Sistem Klasifikasi Sampah</title>
+
+  <link rel="stylesheet" href="../assets/css/maicons.css">
+
+  <link rel="stylesheet" href="../assets/css/bootstrap.css">
+
+  <link rel="stylesheet" href="../assets/vendor/animate/animate.css">
+
+  <link rel="stylesheet" href="../assets/css/theme.css">
+</head>
+<body>
+
+  <!-- Back to top button -->
+  <div class="back-to-top"></div>
+
+  <header>
+  <nav class="navbar navbar-expand-lg navbar-light bg-white sticky" data-offset="500">
+      <div class="container">
+        <a href="#" class="navbar-brand"><span class="text-primary">Kelapah.</span></a>
+
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse collapse" id="navbarContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/blog">Blog</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="/blog">News</a>
+              </li>
+            <li class="nav-item">
+              <a class="btn btn-primary ml-lg-2" href="/classification">Waste Classification</a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="page-banner">
+        <div class="row justify-content-center align-items-center h-100">
+          <div class="col-md-6">
+            <nav aria-label="Breadcrumb">
+              <ul class="breadcrumb justify-content-center py-0 bg-transparent">
+                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item active">News</li>
+              </ul>
+            </nav>
+            <h1 class="text-center">News</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <div class="page-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-10">
+          <form action="#" class="form-search-blog">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <select id="categories" class="custom-select bg-light">
+                  <option>All Categories</option>
+                  <option value="travel">Travel</option>
+                  <option value="lifestyle">LifeStyle</option>
+                  <option value="healthy">Healthy</option>
+                  <option value="food">Food</option>
+                </select>
+              </div>
+              <input type="text" class="form-control" placeholder="Enter keyword..">
+            </div>
+          </form>
+        </div>
+        <div class="col-sm-2 text-sm-right">
+          <button class="btn btn-secondary">Filter <span class="mai-filter"></span></button>
+        </div>
+      </div>
+
+      <div class="page-section">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6 py-3">
+              <h2 class="title-section">Dari Nasabah untuk Nasabah</h2>
+              <div class="post-date">25 Feb 2020</a></div>
+              <div class="divider"></div>
+              <p>Kegiatan BSM melalui pendekatan ekonomi saat ini sudah dapat merubah mindset tentang sampah yang dulunya sumber masalah sekarang telah berubah menjadi sampah adalah rupiah (berkah), merubah perilaku yang dulunya masyarakat membuang sampah ditong sampah ataupun di sungai sudah berubah sampah  [...]</p>
+              <div class="post-date"><a href="#">Read More ...</a></div>
+            </div>
+            <div class="col-lg-6 py-3">
+              <div class="img-fluid py-3 text-center">
+                <img src="../assets/img/1.jpg" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+            <div class="row align-items-center">
+              <div class="col-lg-6 py-3">
+                <h2 class="title-section">Tampil Cantik dengan Busana Berbahan Sampah Plastik</h2>
+                <div class="post-date">Posted On 25 Feb 2019</div>
+                <div class="divider"></div>
+                <p>Malang pojokpitu.com, Limbah rumah tangga atau sampah rumah tangga ternyata bisa dimanfaatkan untuk membuat busana yang indah. Bahkan di Kecamatan Cemorokandang, Kota Malang, digelar fashion show busana berbahan sampah plastik.Aksi warga Perumahan Villa Gunung Buring, RW 8 Kota Malang, yang  [...]</p>
+
+                <div class="post-date"><a href="#">Read More ...</a></div>
+              </div>
+              <div class="col-lg-6 py-3">
+                <div class="img-fluid py-3 text-center">
+                  <img src="../assets/img/2.jpg" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+  </div>
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-6 py-3">
+        <h2 class="title-section">Lewat Bank Sampah Guru Honorer di Malang Kejar Mimpi Raih Gelar Magister</h2>
+        <div class="post-date">Posted on 25 Feb 2019</div>
+        <div class="divider"></div>
+
+        <p>Liputan6.com, Malang - Sampah ibarat dua sisi mata uang. Jika tak ditangani dengan baik pastinya menimbulkan masalah. Di sisi lain, bisa menjadi berkah kalau dikelola dan dimanfaatkan dengan tepat. Bank sampah adalah salah satu sisi pemanfaatan itu. Sulaiman Sulang, staf hubungan masyarakat  [...]</p>
+        <div class="post-date"><a href="#">Read More ...</a></div>
+      </div>
+      <div class="col-lg-6 py-3">
+        <h2 class="title-section">Sim salambim, sampah plastic jadi paling blok warna- warni</h2>
+        <div class="post-date">Posted on 25 Feb 2019</div>
+        <div class="divider"></div>
+        <p>Liputan6.com, Purbalingga - Tumpukan karung berisi sampah plastik menggunung di gudang CV Sipbangga, Karanglewas, Kutasari, Purbalingga, Jawa Tengah. Dua pria sigap memindahkan sampah-sampah plastik ini ke semacam panci besar di atas tungku yang menyala. Setelah mencair, limbah plastik yang tak  [...]</p>
+        <div class="post-date"><a href="#">Read More ...</a></div>
+    </div>
+    </div>
+  </div>
+</div>
+
+      <nav aria-label="Page Navigation">
+        <ul class="pagination justify-content-center">
+          <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item active" aria-current="page">
+            <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
+
+    </div>
+  </div>
+
+  <footer class="page-footer bg-image" style="background-image: url(../assets/img/world_pattern.svg);">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-lg-3 py-3">
+          <h3>SEOGram</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero amet, repellendus eius blanditiis in iusto eligendi iure.</p>
+
+          <div class="social-media-button">
+            <a href="#"><span class="mai-logo-facebook-f"></span></a>
+            <a href="#"><span class="mai-logo-twitter"></span></a>
+            <a href="#"><span class="mai-logo-google-plus-g"></span></a>
+            <a href="#"><span class="mai-logo-instagram"></span></a>
+            <a href="#"><span class="mai-logo-youtube"></span></a>
+          </div>
+        </div>
+        <div class="col-lg-3 py-3">
+          <h5>Company</h5>
+          <ul class="footer-menu">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Career</a></li>
+            <li><a href="#">Advertise</a></li>
+            <li><a href="#">Terms of Service</a></li>
+            <li><a href="#">Help & Support</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 py-3">
+          <h5>Contact Us</h5>
+          <p>203 Fake St. Mountain View, San Francisco, California, USA</p>
+          <a href="#" class="footer-link">+00 1122 3344 5566</a>
+          <a href="#" class="footer-link">seogram@temporary.com</a>
+        </div>
+        <div class="col-lg-3 py-3">
+          <h5>Newsletter</h5>
+          <p>Get updates, news or events on your mail.</p>
+          <form action="#">
+            <input type="text" class="form-control" placeholder="Enter your email..">
+            <button type="submit" class="btn btn-success btn-block mt-2">Subscribe</button>
+          </form>
+        </div>
+      </div>
+
+      <p class="text-center" id="copyright">Copyright &copy; 2020. This template design and develop by <a href="https://macodeid.com/" target="_blank">MACode ID</a></p>
+    </div>
+  </footer>
+
+<script src="../assets/js/jquery-3.5.1.min.js"></script>
+
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+<script src="../assets/js/google-maps.js"></script>
+
+<script src="../assets/vendor/wow/wow.min.js"></script>
+
+<script src="../assets/js/theme.js"></script>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/news', function () {
+    return view('news');
+});
+
+Route::get('/classification', function () {
+    return view('classification');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
